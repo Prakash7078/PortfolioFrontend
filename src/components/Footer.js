477774129acfa889e224c3ce119b6ebe54f20ef8
Prakash7078 <20bq1a05j5@vvit.net>
2023-07-02 14:06:28 +0530

    footer changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,7 @@ import axios from 'axios'
 import { HashLink } from 'react-router-hash-link'
 import { useRef } from 'react'
 import emailjs from '@emailjs/browser'
-
+import svg from '../Images/svg.webp'
 function Footer() {
     const form = useRef();
     const navigate=useNavigate();
@@ -46,31 +46,50 @@ function Footer() {
    
   return (
     <div className='foot'>
-        <div className="col1">
+        <div className='foot-row1'>
+            <img className='svg' src={svg} alt='svg'/>
             <div className="symbols">
-                <Link to="https://github.com/Prakash7078" target="_blank"><img width="30px" height="30px"src={github}/></Link>
-                <Link to="https://www.linkedin.com/feed/" target="_blank"><img width="30px" height="30px"src={linkedin}/></Link>
-                <Link to="https://mern-ecommerce-a2si.onrender.com/" target="_blank"><img width="30px" height="30px"src={cart}/></Link>
-                <Link to="https://www.instagram.com/prakash_chowdary_ponduri/?next=%2F" target="_blank"><img width="30px" height="30px"src={insta}/></Link>
-            </div>
-            <div className="foot-content">
-                <HashLink to="#home">Home</HashLink>
-                <Link to="/contact">Address</Link>
-                <HashLink to="">Services</HashLink>
+                <Link to="https://github.com/Prakash7078" target="_blank"><img width="20px" height="20px"src={github}/></Link>
+                <Link to="https://www.linkedin.com/feed/" target="_blank"><img width="20px" height="20px"src={linkedin}/></Link>
+                <Link to="https://www.instagram.com/prakash_chowdary_ponduri/?next=%2F" target="_blank"><img width="20px" height="20px"src={insta}/></Link>
             </div>
         </div>
         <form ref={form} className='forms'>
-            <h2 >SEND A MESSAGE 💬</h2>
-            <label>Name  </label>
-            <input type="text" name="form_name" onChange={(e)=>{setName(e.target.value)}}></input><br/>
-            <label>Mobile</label>
-            <input type="tel" name="form_tel" onChange={(e)=>{setPhone(e.target.value)}}></input><br/>
-            <label>Email    </label>
-            <input type="email"name="form_email"  onChange={(e)=>{setEmail(e.target.value)}}></input><br/><br/>
-            <label>Message</label>
-            <textarea rows="5" cols="30"placeholder='Message me...'  onChange={(e)=>{setText(e.target.value)}}></textarea> &nbsp;
-            <button id="text"onClick={msgHandler}><img src={what} width="35px" height="35px" /></button>
+            <div className='row1'>
+                <h2 >CONTACT ME 💬</h2>
+                <p>If you'd like to get in touch with me, feel free to reach out using the contact information below or by filling out the contact form.</p>
+            </div>
+            <div className='row2'>
+                <div className='row-col'>
+                    <label>EMAIL</label>
+                    <input type='text' value='ponduriprakash7078@gmail.com' disabled/>
+                </div>
+                <div className='row-col'>
+                    <label>PHONE</label>
+                    <input type='tel' value='+91 9398867753' disabled/>
+                </div>
+            </div>
+            <div className='row3'>
+                <div className='row-col'>
+                    <label>Name</label>
+                    <input type="text" name="form_name" onChange={(e)=>{setName(e.target.value)}}></input>
+                </div>
+                <div className='row-col'>
+                    <label>Mobile</label>
+                    <input type="tel" name="form_tel" onChange={(e)=>{setPhone(e.target.value)}}></input>
+                </div>
+            </div>
+           <div className='row4'>
+                <label>Email</label>
+                <input type="email"name="form_email"  onChange={(e)=>{setEmail(e.target.value)}}></input>
+           </div>
+            <div className='row5'>
+                <label>Message</label>
+                <textarea rows="5" cols="70"placeholder='Message me...'  onChange={(e)=>{setText(e.target.value)}}></textarea> &nbsp;
+            </div>
+            <div className='row6'><button id="text"onClick={msgHandler}>Send Message</button></div>
         </form>
+        <p className='last'>Made with 🤍 by <Link to='https://www.linkedin.com/feed/'>Ponduri Chandra Prakash Babu</Link></p>
     </div>
   )
 }
